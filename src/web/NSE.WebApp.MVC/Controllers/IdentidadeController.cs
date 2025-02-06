2f6dea0fa238a034fc48f2e2bc5af8d148724617
santtose <santtose@outlook.com>
2025-02-06 15:49:51 -0300

    Model, Views e Controllers de login

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,8 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+            //var resposta = await _autenticacaoService.Registro(usuarioRegistro);
+
+            //if (ResponsePossuiErros(resposta.ResponseResult)) return View(usuarioRegistro);
+
+            //await RealizarLogin(resposta);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            //ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin, string returnUrl = null)
+        {
+            //ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //var resposta = await _autenticacaoService.Login(usuarioLogin);
+
+            //if (ResponsePossuiErros(resposta.ResponseResult)) return View(usuarioLogin);
+
+            //await RealizarLogin(resposta);
+
+            //if (string.IsNullOrEmpty(returnUrl)) return RedirectToAction("Index", "Home");
+
+            //return LocalRedirect(returnUrl);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
