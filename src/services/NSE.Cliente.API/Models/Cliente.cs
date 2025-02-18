da3ff92d8648e847ead6cad3f3a637965864ff55
santtose <santtose@outlook.com>
2025-02-18 14:55:09 -0300

    definindo entidades

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente : Entity, IAgregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+
+        // EF Relation
+        protected Cliente() { }
+
+        public Cliente(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+    }
+}
