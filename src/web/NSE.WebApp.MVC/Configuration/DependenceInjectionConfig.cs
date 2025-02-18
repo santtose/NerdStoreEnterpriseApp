b15b3c981f89058e26f0c84915cb9aae35439b03
santtose <santtose@outlook.com>
2025-02-18 11:32:20 -0300

    Retry Pattern com Polly

--- src/web/NSE.WebApp.MVC/Configuration/DependenceInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependenceInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -12,13 +14,28 @@ namespace NSE.WebApp.MVC.Configuration
             services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
 
             // Registro do httpClient
-            services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();              
+            services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            var retryWaitPolicy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4),
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddPolicyHandler(retryWaitPolicy);
 
             // Cada request é uma representação unica. Uma única instância do serviço para toda a aplicação
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
             // Base no request especifico. Cria uma única instância por requisição (scope).
             services.AddScoped<IUser, AspNetUser>();
