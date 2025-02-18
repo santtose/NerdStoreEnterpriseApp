49d2b3a37c895ba9cb3a3db4ce50d03175969cc1
santtose <santtose@outlook.com>
2025-02-18 15:15:06 -0300

    Objetos de valor e validacao

--- src/services/NSE.Cliente.API/Models/Cliente.cs
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -5,9 +5,10 @@ namespace NSE.Cliente.API.Models
     public class Cliente : Entity, IAgregateRoot
     {
         public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
 
         // EF Relation
         protected Cliente() { }
@@ -16,9 +17,19 @@ namespace NSE.Cliente.API.Models
         {
             Id = id;
             Nome = nome;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
         }
     }
 }
