3dd80afaac52bf82fad79fcabaa827c8b3b01d21
santtose <santtose@outlook.com>
2025-02-18 16:14:25 -0300

    Mapeamento de tabelas com Entity

---
+++ src/services/NSE.Cliente.API/Configuration/ApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Clientes.API.Data;
+using NSE.WebAPI.Core.Identidade;
+
+namespace NSE.Clientes.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ClientesContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.MapControllers();
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Clientes.API.Models;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Clientes.API.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            // É feito assim pois o cpf é uma classe, e numero é uma prop dessa classe
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Numero)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            });
+            // Mesma coisa que o cpf
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Endereco)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.EnderecoMaxLength})");
+            });
+
+            // 1 : 1 => Aluno : Endereco
+            builder.HasOne(c => c.Endereco)
+                .WithOne(c => c.Cliente);
+
+            builder.ToTable("Clientes");
+        }
+    }
+}
