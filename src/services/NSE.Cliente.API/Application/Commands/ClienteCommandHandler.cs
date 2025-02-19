d9484caa3e59820aacc16b99db7fc4ec49a43a00
santtose <santtose@outlook.com>
2025-02-19 11:14:23 -0300

    Persistencia via command handler

--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -9,19 +9,30 @@ namespace NSE.Clientes.API.Application.Commands
     // IRequestHandler - Manipulador do request
     public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteCommandHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
         public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
         {
             if (!message.EhValido()) return message.ValidationResult;
 
             var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
 
-            if (true)
+            var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
+
+            if (clienteExistente != null)
             {
                 AdicionarErro("Este CPF já está em uso.");
                 return ValidationResult;
             }
 
-            return message.ValidationResult;
+            _clienteRepository.Adicionar(cliente);
+
+            return await PersistirDados(_clienteRepository.UnitOfWork);
         }
     }
 }
--- src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,9 @@
 ﻿using FluentValidation.Results;
 using MediatR;
 using NSE.Clientes.API.Application.Commands;
+using NSE.Clientes.API.Data;
+using NSE.Clientes.API.Data.Repository;
+using NSE.Clientes.API.Models;
 using NSE.Core.Mediator;
 
 namespace NSE.Clientes.API.Configuration
@@ -12,6 +15,9 @@ namespace NSE.Clientes.API.Configuration
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             // Registra que o RegistrarClienteCommand será entregue via IRequestHandler e que vai retornar um ValidationResult será manipulado pelo ClienteCommandHandler, isso via MediaTr
             services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
+
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<ClientesContext>();
         }
     }
 }
