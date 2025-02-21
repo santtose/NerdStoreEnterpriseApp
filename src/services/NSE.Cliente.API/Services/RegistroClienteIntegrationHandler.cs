d97f094f1eff498ebe3b84225f33270147a842db
santtose <santtose@outlook.com>
2025-02-21 11:15:44 -0300

    Refatorando o RegistroClienteIntegrationHandler e a AuthController onde chama o Bus

--- src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
+++ src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
@@ -1,35 +1,32 @@
-﻿
-using EasyNetQ;
+﻿using EasyNetQ;
 using FluentValidation.Results;
 using NSE.Clientes.API.Application.Commands;
 using NSE.Core.Mediator;
 using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
 
 namespace NSE.Clientes.API.Services
 {
     // Manipula a integração trabalha como um background service
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
-        private IBus _bus;
+        private IMessageBus _bus;
         private readonly IServiceProvider _serviceProvider;
 
-        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider, IMessageBus bus)
         {
             _serviceProvider = serviceProvider;
+            _bus = bus;
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _bus = RabbitHutch.CreateBus("host=localhost");
+            await _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(RegistrarCliente);
 
-            _bus.Rpc.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request =>
-                new ResponseMessage(await RegistrarCliente(request)));
-
-            return Task.CompletedTask;
-            
+            await Task.CompletedTask;            
         }
 
-        private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
+        private async Task<ResponseMessage> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
         {
             ValidationResult sucesso;
 
@@ -42,7 +39,7 @@ namespace NSE.Clientes.API.Services
                 sucesso = await mediator.EnviarComando(clienteCommand);
             }
 
-            return sucesso;
+            return new ResponseMessage(sucesso);
         }
     }
 }
