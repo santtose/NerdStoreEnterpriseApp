c1073d900fa6faa47daef9129e0e393145b71b4c
santtose <santtose@outlook.com>
2025-02-25 09:55:02 -0300

    Iniciando configuracoes da api de carrinho

---
+++ src/services/NSE.Carrinho.API/Configuration/ApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Carrinho.API.Data;
+using NSE.WebAPI.Core.Identidade;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CarrinhoContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.MapControllers();
+        }
+    }
+}
---
+++ src/services/NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using NSE.Carrinho.API.Data;
+
+namespace NSE.Carrinho.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<CarrinhoContext>();
+        }
+    }
+}
