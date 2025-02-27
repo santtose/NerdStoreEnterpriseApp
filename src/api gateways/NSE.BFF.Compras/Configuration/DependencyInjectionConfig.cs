3c793ee82a854116ed737a98e4294a99993d5310
santtose <santtose@outlook.com>
2025-02-27 15:05:16 -0300

    resolvendo injecao

--- src/api gateways/NSE.BFF.Compras/Configuration/DependencyInjectionConfig.cs
+++ src/api gateways/NSE.BFF.Compras/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,8 @@
-﻿using NSE.WebAPI.Core.Usuario;
+﻿using NSE.BFF.Compras.Extensions;
+using NSE.BFF.Compras.Services;
+using NSE.WebAPI.Core.Extensions;
+using NSE.WebAPI.Core.Usuario;
+using Polly;
 
 namespace NSE.BFF.Compras.Configuration
 {
@@ -8,6 +12,20 @@ namespace NSE.BFF.Compras.Configuration
         {
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AddTransientHttpErrorPolicy(
+                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
+            services.AddHttpClient<ICarrinhoService, CarrinhoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AddTransientHttpErrorPolicy(
+                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
         }
     }
 }
