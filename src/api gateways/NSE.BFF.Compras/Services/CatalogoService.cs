fec8f65041ef7c77498e6577badab4072f9c3a57
santtose <santtose@outlook.com>
2025-02-27 15:02:27 -0300

    Consumindo servicos pela BFF

--- src/api gateways/NSE.BFF.Compras/Services/CatalogoService.cs
+++ src/api gateways/NSE.BFF.Compras/Services/CatalogoService.cs
@@ -1,20 +1,44 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.BFF.Compras.Extensions;
+using NSE.BFF.Compras.Models;
 
 namespace NSE.BFF.Compras.Services
 {
     public interface ICatalogoService
     {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
+        Task<IEnumerable<ItemProdutoDTO>> ObterItens(IEnumerable<Guid> ids);
     }
 
     public class CatalogoService : Service, ICatalogoService
     {
         private readonly HttpClient _httpClient;
 
+
         public CatalogoService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+        }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
+        }
+
+        public async Task<IEnumerable<ItemProdutoDTO>> ObterItens(IEnumerable<Guid> ids)
+        {
+            var idsRequest = string.Join(",", ids);
+
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/lista/{idsRequest}/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ItemProdutoDTO>>(response);
         }
     }
 }
