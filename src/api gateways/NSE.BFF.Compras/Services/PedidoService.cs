67c7866d1898e142e29490f899d34d967af5f946
santtose <santtose@outlook.com>
2025-03-06 16:12:27 -0300

    Aplicando voucher via BFF

--- src/api gateways/NSE.BFF.Compras/Services/PedidoService.cs
+++ src/api gateways/NSE.BFF.Compras/Services/PedidoService.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.BFF.Compras.Extensions;
+using NSE.BFF.Compras.Models;
+using System.Net;
 
 namespace NSE.BFF.Compras.Services
 {
     public interface IPedidoService
     {
+        Task<VoucherDTO> ObterVoucherPorCodigo(string codigo);
     }
 
     public class PedidoService : Service, IPedidoService
@@ -15,6 +18,17 @@ namespace NSE.BFF.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.PedidoUrl);
+        }
+
+        public async Task<VoucherDTO> ObterVoucherPorCodigo(string codigo)
+        {
+            var response = await _httpClient.GetAsync($"/voucher/{codigo}/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<VoucherDTO>(response);
         }
     }
 }
--- src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,8 @@
 ﻿using NSE.Core.Mediator;
+using NSE.Pedidos.API.Application.Queries;
+using NSE.Pedidos.Domain.Vouchers;
 using NSE.Pedidos.Infra.Data;
+using NSE.Pedidos.Infra.Data.Repository;
 using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Pedidos.API.Configuration
@@ -8,13 +11,16 @@ namespace NSE.Pedidos.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            // API
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
 
             //Data
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
         }
     }
