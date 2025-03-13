9406e4b400c57889364178c7ba04c6c4bae2618e
santtose <santtose@outlook.com>
2025-03-13 10:44:22 -0300

    Finalizando api de pedido

--- src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,6 @@
 ﻿using NSE.Core.Mediator;
 using NSE.Pedidos.API.Application.Queries;
+using NSE.Pedidos.Domain.Pedidos;
 using NSE.Pedidos.Domain.Vouchers;
 using NSE.Pedidos.Infra.Data;
 using NSE.Pedidos.Infra.Data.Repository;
@@ -15,11 +16,19 @@ namespace NSE.Pedidos.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Commands e Events
+            // Por meio de DI do MediatR ele encontra os commands e handler via reflefction e ele mesmo os registra, isso se os
+            // objetosestiverem no mesmo projeto ondce a DI é realizada. Caso contrário você precisa registrar.
+
+            //services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+            //services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
 
             //Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
         }
