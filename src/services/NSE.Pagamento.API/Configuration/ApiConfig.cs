1adc1ae90ff2a8ef116e74a0f96fc6611ccbae1f
santtose <santtose@outlook.com>
2025-03-24 16:04:29 -0300

    Configuracoes iniciais

---
+++ src/services/NSE.Pagamento.API/Configuration/ApiConfig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Pagamentos.API.Data;
+using NSE.WebAPI.Core.Identidade;
+
+namespace NSE.Pagamentos.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PagamentosContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseCors("Total");
+
+            app.UseAuthConfiguration();
+
+            app.MapControllers();
+        }
+    }
+}
---
+++ src/services/NSE.Pagamento.API/Data/Repository/PagamentoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Pagamentos.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using NSE.Core.Data;
+
+namespace NSE.Pagamentos.API.Data.Repository
+{
+    public class PagamentoRepository : IPagamentoRepository
+    {
+        private readonly PagamentosContext _context;
+
+        public PagamentoRepository(PagamentosContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void AdicionarPagamento(Pagamento pagamento)
+        {
+            _context.Pagamentos.Add(pagamento);
+        }
+
+        public void AdicionarTransacao(Transacao transacao)
+        {
+            _context.Transacoes.Add(transacao);
+        }
+
+        public async Task<Pagamento> ObterPagamentoPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Pagamentos.AsNoTracking()
+                .FirstOrDefaultAsync(p => p.PedidoId == pedidoId);
+        }
+
+        public async Task<IEnumerable<Transacao>> ObterTransacaoesPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Transacoes.AsNoTracking()
+                .Where(t => t.Pagamento.PedidoId == pedidoId).ToListAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
