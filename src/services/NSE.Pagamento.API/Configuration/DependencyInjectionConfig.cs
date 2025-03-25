a64847b4201774247dff04197471e0e61afab00d
santtose <santtose@outlook.com>
2025-03-25 10:18:47 -0300

    Background service de pagamento

--- src/services/NSE.Pagamento.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pagamento.API/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Pagamentos.API.Data;
+using NSE.Pagamentos.API.Data.Repository;
+using NSE.Pagamentos.API.Facade;
+using NSE.Pagamentos.API.Models;
+using NSE.Pagamentos.API.Services;
 using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Pagamentos.API.Configuration
@@ -12,6 +16,10 @@ namespace NSE.Pagamentos.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            services.AddScoped<IPagamentoService, PagamentoService>();
+            services.AddScoped<IPagamentoFacade, PagamentoCartaoCreditoFacade>();
+
+            services.AddScoped<IPagamentoRepository, PagamentoRepository>();
             services.AddScoped<PagamentosContext>();
         }
     }
---
+++ src/services/NSE.Pagamento.API/Services/IPagamentoService.cs
@@ -0,0 +1,11 @@
+﻿using NSE.Core.Messages.Integration;
+using NSE.Pagamentos.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.API.Services
+{
+    public interface IPagamentoService
+    {
+        Task<ResponseMessage> AutorizarPagamento(Pagamento pagamento);
+    }
+}
