bd512250c6f4b37ff5265f23b5ce12de16e7cb61
santtose <santtose@outlook.com>
2025-03-25 09:30:04 -0300

    Iniciando facade para gateway de pagamento

---
+++ src/services/NSE.Pagamento.API/Facade/IPagamentoFacade.cs
@@ -0,0 +1,10 @@
+﻿using NSE.Pagamentos.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.API.Facade
+{
+    public interface IPagamentoFacade
+    {
+        Task<Transacao> AutorizarPagamento(Pagamento pagamento);
+    }
+}
