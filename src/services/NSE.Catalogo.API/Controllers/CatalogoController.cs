40dee2043fbd38ab2ad082a62c99463f468c4084
santtose <santtose@outlook.com>
2025-03-31 10:37:54 -0300

    Paginacao pela API catalogo

--- src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Catalogo.API.Models;
 using NSE.WebAPI.Core.Controllers;
 
@@ -12,11 +13,12 @@ namespace NSE.Catalogo.API.Controllers
         {
             _produtoRepository = produtoRepository;
         }
-                
+
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
-        public async Task<IEnumerable<Produto>> Index()
+        public async Task<PagedResult<Produto>> Index([FromQuery] int ps = 8, [FromQuery] int page = 1, [FromQuery] string q = null)
         {
-            return await _produtoRepository.ObterTodos();
+            return await _produtoRepository.ObterTodos(ps, page, q);
         }
                
         [HttpGet("catalogo/produtos/{id}")]
